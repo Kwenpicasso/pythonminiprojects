c486d44726cd77cd71a154076d9e19d806ce8e98
Anikwenze Emmanuel <milezkwen@gmail.com>
2025-04-15 11:46:51 +0100

    ft/word-scrambler

---
+++ word-scrambler/app.py
@@ -0,0 +1,12 @@
+import random
+print("WORD SCRAMBLER")
+
+while True:
+    value = input("Enter a word to be scrambled (or quit): ")
+    if value.lower() == 'quit':
+        print("Goodbye")
+        break
+    letter = list(value)
+    random.shuffle(letter)
+    print(f"scrambled: {''.join(letter)}")
+        
